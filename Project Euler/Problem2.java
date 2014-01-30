6b982671e52492633501995f4af57fe86a688062
Zarthus <BuuGhost@gmail.com>
2014-01-30 22:05:11 +0100

    'Cause we all love array lists that show what is actually in the array.

--- Project Euler/Problem2.java
+++ Project Euler/Problem2.java
@@ -1,5 +1,7 @@
 package nl.zarthus.euler;
 
+import java.util.ArrayList;
+
 public class Problem2 extends Euler
 {
 	/*
@@ -41,6 +43,37 @@ public class Problem2 extends Euler
 		} while (b <= 4000000);
 
 		System.out.println("Problem 2: " + sum);
+	}
+	
+	public void solveTwo()
+	{
+		// MAX = 4 million as specified by assignment
+		int sum = 0, a = 0, b = 1, tmp = 0;
+		ArrayList<Integer> allEven = new ArrayList<Integer>(),
+				allNums = new ArrayList<Integer>();
+		
+		// Sum 	= total sum, and our answer for this assignment
+		// A 	= the variable that holds the first number
+		// B 	= the second variable that will always be 'a + b' on next iteration
+		// tmp 	= temporary storage of A to calculate with.
+		
+		do 
+		{
+			tmp = a + b;
+			a = b;
+			b = tmp;
+			allNums.add(b);
+			
+			if (isEven(b))
+			{
+				sum = sum += b;
+				allEven.add(sum);
+			}
+		} while (b <= 4000000);
+
+		System.out.println("Problem 2: " + sum);
+		System.out.println("Problem 2 - Array (0) [all numbers]: " + allNums.toString());
+		System.out.println("Problem 2 - Array (1) [new sum when even]: " + allEven.toString());
 	}
 
 	public boolean isEven(int value) // Check is a value is even by getting its remainder
