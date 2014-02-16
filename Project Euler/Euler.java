a0f510fc5856265a94e69864e27bb31c5012e2a0
Zarthus <BuuGhost@gmail.com>
2014-02-16 17:32:45 +0100

    Class Euler now supports notifying of unoptimal solutions, any issue by default is optimal and will not trigger the warning.

--- Project Euler/Euler.java
+++ Project Euler/Euler.java
@@ -1,26 +1,33 @@
 package nl.zarthus.euler;
 
+
 public class Euler
 {
-	public boolean SOLVED = false;
-
+	public boolean	SOLVED				= false;
+	public boolean	OPTIMAL_SOLUTION	= true;
+	
 	public void solve()
 	{
 		System.out.println("Still using default Euler method solve()");
 	}
-
+	
 	public void solve(int param)
 	{
 		System.out.println("Calling solve() on problem that does not support parameters.");
 	}
-
+	
 	public boolean getSolved()
 	{
 		return this.SOLVED;
 	}
-
+	
 	public void printSolvedString()
 	{
 		System.out.println("This problem is " + (this.SOLVED ? "" : "not yet ") + "solved.");
+		if (this.SOLVED && !this.OPTIMAL_SOLUTION)
+		{
+			System.out.println("!! This problem has issues: The solution is far from optimal. !!");
+		}
 	}
 }
+
