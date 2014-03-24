f3f594e9893f6cb4db8aa7a0d02f95397a25efe3
Jos Ahrens <BuuGhost@gmail.com>
2014-03-24 13:58:41 +0100

    Add problem template to copy from

---
+++ Project Euler/ProblemTemplate.java
@@ -0,0 +1,21 @@
+package nl.zarthus.euler;
+
+public class ProblemTemplate extends Euler
+{
+	/*
+	 * 
+	 * 
+	 * 
+	 */
+
+	public ProblemTemplate()
+	{
+		this.SOLVED = false;
+		this.SOLUTION_STATE = Euler.SOLUTION_OPTIMAL;
+	}
+
+	@Override
+	public void solve()
+	{
+	}
+}
