f7b5f6db168d9019d45b3038ba268bd4e87ea708
Zarthus <zarthus@zarth.us>
2014-05-12 11:35:20 +0200

    Assembly code

--- Assembly/assignment4.asm
+++ Assembly/assignment4.asm
@@ -4,10 +4,13 @@ $include(system_includes.asm)
 ;	@date	09/04/2014
 
 start:	MOV	A,P0	; Read the switch
-	MOV	B,#2	; Read switch again.
+
+	ADD	A,#18	; Add 18 to A
+	MOV 	P1,A	; Move A to P1
 	
-	MUL	AB	; multiply A
+	;... todo (page 21)
 	
+
 	MOV	P1,A	; A on the exit port.
 	JMP	start	; 
 	
