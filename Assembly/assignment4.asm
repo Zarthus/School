cff5f3110ce36ce3ee99f2c435785b53a991c990
Zarthus <zarthus@zarth.us>
2014-06-04 09:31:00 +0200

    Finish assignment #4

--- Assembly/assignment4.asm
+++ Assembly/assignment4.asm
@@ -4,7 +4,7 @@ $include(system_includes.asm)
 ;	@date	14/05/2014
 
 start:	MOV	A,P0	; Read the switch
-	JMP 	case3	; Select which case to run.
+	JMP	case4	; Select which case to run.
 
 case0:	; CASE 0 -- add 18 to P0 and put it in P1. Compiles & works
 
@@ -15,12 +15,12 @@ case0:	; CASE 0 -- add 18 to P0 and put it in P1. Compiles & works
 case1:	; CASE 1 -- multiply P0 by 2, and minus 10, put it in P1. Compiles & works
 
 	MOV	B,P0	; Move P0 into B
-	ADD 	A,B	; Multiply A by A (B == A)
-	SUBB 	A,#10	; Subtract 10 from A
+	ADD	A,B	; Multiply A by A (B == A)
+	SUBB	A,#10	; Subtract 10 from A
 	
 	JMP	eop	; Go to end of program
 	
-case2:	; CASE 2 -- Multiply P0+5 by 3. Doesn't compile therefore doesn't work.
+case2:	; CASE 2 -- Multiply P0+5 by 3. 
 	ADD	A,#5	; Add 3 to A
 	MOV	B,A	; B = A
 	ADD	A,B	; Multiply B by 3
@@ -28,18 +28,18 @@ case2:	; CASE 2 -- Multiply P0+5 by 3. Doesn't compile therefore doesn't work.
 	
 	JMP	eop	; Go to end of program
 	
-case3:	; CASE 3 -- P0 to the power of 2. Doesn't compile
-	MOV	A,B	; B = A
-	MUL 	AA	; A to the power of 2 is simply A * A 
+case3:	; CASE 3 -- P0 to the power of 2. 
+	MOV	B,A	; B = A
+	MUL	AB	; A to the power of 2 is simply A * A 
 
 	JMP	eop	; Go to end of program
 	
 case4:	; CASE 4 -- P0 + 256
-	;MOV	A,P0	; Move P0 into A. No compile
-	;ADD	A,#256	; Add 256 to A
+	
+	ADD	A,#128	; Add 256 to A
+	ADD	A,#128
 	
 	JMP	eop	; Go to end of program
-	;... todo (page 21)
 	
 eop:
 	MOV	P1,A	; A on the exit port. - Avoid duplicate code by doing it at the end of the program
